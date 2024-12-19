f2402ffdb95f7d1c6efb7e41933fce5b2bade171
SOURAV KANWAR <souravkanwar007@gmail.com>
2024-12-19 16:53:58 +0530

    Add files via upload

---
+++ Audiophile/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      sans: ['Manrope', 'system-ui', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
